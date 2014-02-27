7a2f320737ff1bd9832790467b93038d4a492164
Ryan Leahy <ryanleahy90@gmail.com>
2014-02-27 14:16:27 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Liftonomics::Application.config.secret_key_base = 'e5511ccf7cc55188822d350bd08ac812510eb8b4fd04ac8611699e3e6bac4987c2cf272e41c58a6f3973802e3f5ec463ade1f933fb5fec95c0f2a4588104fe6e'
